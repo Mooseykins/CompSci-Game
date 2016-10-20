d71dddd37461b8e87e12910b5209f0fc2fc8e7c0
Mooseykins <careydawson34@gmail.com>
2016-10-20 08:10:19 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Importations
+import java.util.*
+import java.applet.Applet
+// by Dawson carey
+// This should be the engine file
+public class Main
+{
+public static void main(String[] args);
+{
+//Declarations
+
+//Engine start
